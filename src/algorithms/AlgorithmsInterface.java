efbbc693ad0fa5288bd4cf444449fd530f74ebeb
Robin Gaur <robin.gaur26@gmail.com>
2020-01-12 13:33:01 +0530

    just removing comments

--- src/algorithms/AlgorithmsInterface.java
+++ src/algorithms/AlgorithmsInterface.java
@@ -5,7 +5,4 @@ public interface AlgorithmsInterface {
     void setAnimationDelay(double delay);
 
     double getAnimationDelay();
-//    public void sleep();
-//    public boolean isSleep();
-//    public void awake();
 }
