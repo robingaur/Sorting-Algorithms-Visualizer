3e143ece86b1a638d044a7d7cd07fb1d6e843a09
Robin Gaur <robin.gaur26@gmail.com>
2020-01-19 02:46:05 +0530

    merge sort bugs fixed

--- src/algorithms/MergeSort.java
+++ src/algorithms/MergeSort.java
@@ -27,58 +27,53 @@ public class MergeSort extends AbstractAlgorithms {
     }
 
     private void merge(int start, int mid, int end) {
-        try {
-            // Sizes of the Array
-            int sizeOfLeftArray = mid - start + 1;
-            int sizeOfRightArray = end - mid;
+        // Sizes of the Array
+        int sizeOfLeftArray = mid - start + 1;
+        int sizeOfRightArray = end - mid;
 
-            int[] left = new int[sizeOfLeftArray];
-            int[] right = new int[sizeOfRightArray];
+        int[] left = new int[sizeOfLeftArray];
+        int[] right = new int[sizeOfRightArray];
 
-            for (int i = 0; i < sizeOfLeftArray; i++) {
-                left[i] = super.list.get(start + i).intValue();
-            }
+        for (int i = 0; i < sizeOfLeftArray; i++) {
+            left[i] = super.list.get(start + i).intValue();
+        }
 
-            for (int i = 0; i < sizeOfRightArray; i++) {
-                right[i] = super.list.get(mid + i + 1).intValue();
-            }
+        for (int i = 0; i < sizeOfRightArray; i++) {
+            right[i] = super.list.get(mid + i + 1).intValue();
+        }
 
-            int i = 0, j = 0, k = start;
-            while (i < sizeOfLeftArray && j < sizeOfRightArray) {
-                super.drawAndDeleteRedRectangle(k, k);
+        int i = 0, j = 0, k = start;
+        while (i < sizeOfLeftArray && j < sizeOfRightArray) {
+            super.drawAndDeleteRedRectangle(k, k);
 
-                if (left[i] <= right[j]) {
-                    super.list.set(k, left[i]);
-                    i++;
-                } else {
-                    super.list.set(k, right[j]);
-                    j++;
-                }
-
-                super.drawGreenRectangle(k, k);
-                Thread.sleep(10);
-                k++;
-            }
-
-            while (i < sizeOfLeftArray) {
-                super.drawAndDeleteRedRectangle(k, k);
-                this.list.set(k, left[i]);
+            if (left[i] <= right[j]) {
+                super.list.set(k, left[i]);
                 i++;
-
-                super.drawGreenRectangle(k, k);
-                k++;
-            }
-
-            while (j < sizeOfRightArray) {
-                super.drawAndDeleteRedRectangle(k, k);
-                this.list.set(k, right[j]);
+            } else {
+                super.list.set(k, right[j]);
                 j++;
-
-                super.drawGreenRectangle(k, k);
-                k++;
             }
-        } catch (InterruptedException ex) {
-            ex.printStackTrace();
+
+            super.drawGreenRectangle(k, k);
+            k++;
+        }
+
+        while (i < sizeOfLeftArray) {
+            super.drawAndDeleteRedRectangle(k, k);
+            this.list.set(k, left[i]);
+            i++;
+
+            super.drawGreenRectangle(k, k);
+            k++;
+        }
+
+        while (j < sizeOfRightArray) {
+            super.drawAndDeleteRedRectangle(k, k);
+            this.list.set(k, right[j]);
+            j++;
+
+            super.drawGreenRectangle(k, k);
+            k++;
         }
     }
 }
---
+++ src/algorithms/MergeSortWithConstantSpace.java
@@ -0,0 +1,84 @@
+package algorithms;
+
+import UtilityClasses.RectangleInterface;
+
+import java.util.List;
+
+public class MergeSortWithConstantSpace extends AbstractAlgorithms {
+
+    public MergeSortWithConstantSpace(List<Number> list, RectangleInterface rectangle) {
+        super(list, rectangle);
+    }
+
+    @Override
+    protected void sort() {
+        this.sort(0, this.list.size() - 1);
+    }
+
+    private void sort(int start, int end) {
+        if (start < end) {
+            int mid = (start + end) / 2;
+
+            this.sort(start, mid);
+            this.sort(mid + 1, end);
+
+            this.merge(start, mid, end);
+        }
+    }
+
+    private void merge(int start, int mid, int end) {
+        try {
+            // Sizes of the Array
+            int sizeOfLeftArray = mid - start + 1;
+            int sizeOfRightArray = end - mid;
+
+            int[] left = new int[sizeOfLeftArray];
+            int[] right = new int[sizeOfRightArray];
+
+            for (int i = 0; i < sizeOfLeftArray; i++) {
+                left[i] = super.list.get(start + i).intValue();
+            }
+
+            for (int i = 0; i < sizeOfRightArray; i++) {
+                right[i] = super.list.get(mid + i + 1).intValue();
+            }
+
+            int i = 0, j = 0, k = start;
+            while (i < sizeOfLeftArray && j < sizeOfRightArray) {
+                super.drawAndDeleteRedRectangle(k, k);
+
+                if (left[i] <= right[j]) {
+                    super.list.set(k, left[i]);
+                    i++;
+                } else {
+                    super.list.set(k, right[j]);
+                    j++;
+                }
+
+                super.drawGreenRectangle(k, k);
+                Thread.sleep(10);
+                k++;
+            }
+
+            while (i < sizeOfLeftArray) {
+                super.drawAndDeleteRedRectangle(k, k);
+                this.list.set(k, left[i]);
+                i++;
+
+                super.drawGreenRectangle(k, k);
+                k++;
+            }
+
+            while (j < sizeOfRightArray) {
+                super.drawAndDeleteRedRectangle(k, k);
+                this.list.set(k, right[j]);
+                j++;
+
+                super.drawGreenRectangle(k, k);
+                k++;
+            }
+        } catch (InterruptedException ex) {
+            ex.printStackTrace();
+        }
+    }
+}
